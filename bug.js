19286a52fe5d15ac0c45662140d4ebd5752f9e3a
BugHunter-X <askzenai@gmail.com>
2024-12-25 08:28:07 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This error occurs when using the Expo CLI and attempting to run an app that uses a library or feature that is not properly configured or compatible with the current Expo SDK version.  For instance, using a library that requires a specific native module that hasn't been properly linked or a feature that is not supported by the current SDK version.
